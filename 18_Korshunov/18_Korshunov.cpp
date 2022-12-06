a4d2dc258f28288eca138a61e8ac86a007984a73
gg-master <korshun.tolik@yandex.ru>
2022-12-06 13:42:44 +0300

    Начал прописывать реализацию доп функций

--- 18_Korshunov/18_Korshunov.cpp
+++ 18_Korshunov/18_Korshunov.cpp
@@ -2,40 +2,81 @@
 #include "../18_Korshunov/18_Korshunov.h"
 #include <cstring>
 
+#pragma warning(disable : 4996)
 
 
-int main()
+/*! Удалить перечисленные символы слева от строки
+*/
+char* stripLeft(char* string, const char stripBy[]="\\t\\n\\v\\f\\r ")
 {
-	char inputText[][MAX_LINE_LENGTH+1] =
-	{
-		"int main(a)",
-		"{",
-		"}",
-	};
-	char targetFuncName[81] = "main"; 
-
-	Text programText = { {}, 3 };
-	for (int i = 0; i < programText.countLines; i++)
-		programText.ptrOnLines[i] = (char*)inputText[i];
-
-	Text funcDefinition = {};
-
-	bool resOfSearch = findFunctionDefinition(programText, targetFuncName, &funcDefinition);
-	printf("%d\n", resOfSearch);
-	for (int i = 0; i < funcDefinition.countLines; i++)
-	{
-		printf("%s\n", funcDefinition.ptrOnLines[i]);
-	}
+	return string + strspn(string, stripBy) - 1;
 }
 
-bool isWordInLine(const char *line, const char *word)
+/*! Удалить перечисленные символы справа от строки
+*/
+char* stripRight(char* string, const char stripBy[]="\\t\\n\\v\\f\\r ")
+{
+	int i = 0;
+	while (*(string + i) != '\0') i++;
+	if (i > 0)
+		while (i-- > 0 && (strchr(stripBy, (*(string + i))) != NULL))
+			*(string + i) = '\0';
+	return string;
+}
+
+/* Удалить перечисленные символы и слева и справа
+*/
+char* strip(char* string, const char stripBy[]="\\t\\n\\v\\f\\r ")
+{
+	return stripLeft(stripRight(string, stripBy), stripBy);
+}
+
+/* Удалить однострочный комментарий из строки
+*/
+char* delOneLineComment(char* string)
+{
+	char* result = strstr(string, "//");
+	if (result != NULL) *result = '\0';
+	return string;
+}
+
+
+bool isWordInLine(const char* line, const char* word)
 {
 	return strstr(line, word) != NULL;
 }
 
-bool isLineHeadOfDefinition(const char line[], const char word[])
+//bool isCompareLineWithPattern(const char* string, const char* pattern)
+//{
+//	char tmp_s[MAX_LINE_LENGTH+1] = "", tmp_p[MAX_LINE_LENGTH + 1] = "";
+//	char* ptr_s = strcpy(tmp_s, string);
+//	char* ptr_p = strcpy(tmp_p, pattern);
+//	
+//	char prev_p = NULL;
+//	while (ptr_s != NULL && *ptr_p != '\0')
+//	{
+//		if (*ptr_p == '#')
+//		{
+//			if (prev_p == NULL && (*tmp_s != *(ptr_p + 1))) return false;
+//			if (prev_p != NULL && *(ptr_s - 1) != prev_p && *ptr_s != *(ptr_p + 1)) return false;
+//			ptr_p++;
+//		}
+//		ptr_s = strchr(ptr_s, *ptr_p); 
+//		if (ptr_s != NULL) ptr_s++;
+//		prev_p = *ptr_p; ptr_p++;
+//		
+//	}
+//	return (ptr_s == NULL) ? false : true;
+//}
+
+bool isLineHeadOfDefinition(const char* string, const char* funcName)
 {
-	if (isWordInLine(line, word))
+	char tmp[MAX_LINE_LENGTH + 1] = ""; 
+	strcpy(tmp, string);
+	char* tmp_cleared = strip(delOneLineComment(tmp));
+	//if (isCompareLineWithPattern(tmp_cleared, )
+	char* ptrOnFuncNameInStr = strstr(tmp_cleared, funcName);
+	if (*(tmp_cleared + strlen(tmp_cleared) -1) == ')' && *(ptrOnFuncNameInStr + strlen(funcName)) == '(' && tmp_cleared != ptrOnFuncNameInStr)
 	{
 		// логика определения 
 		return true;
@@ -43,15 +84,24 @@ bool isLineHeadOfDefinition(const char line[], const char word[])
 	return false;
 }
 
-bool isLineEndOfDefinition(const char line[])
-{	
-	return strstr(line, "}") != NULL;
+int findHeadOfDefinition(const Text programText, const char targetFuncName[MAX_LINE_LENGTH + 1])
+{
+	// Для каждой строки текста, пока не найдена искомая функция
+	for (int i = 0; i < programText.countLines; i++)
+	{
+		//
+		if (isWordInLine(programText.ptrOnLines[i], targetFuncName) && 
+			isLineHeadOfDefinition(programText.ptrOnLines[i], targetFuncName))
+		{
+			return i;
+		}
+	}
 }
 
-//char* strip(char* string)
-//{
-//
-//}
+bool isLineEndOfDefinition(const char line[])
+{
+	return strstr(line, "}") != NULL;
+}
 
 bool findFunctionDefinition(const Text programText, const char targetFuncName[MAX_LINE_LENGTH + 1], Text* funcDefinition)
 {
@@ -61,7 +111,7 @@ bool findFunctionDefinition(const Text programText, const char targetFuncName[MA
 	for (int i = 0; i < programText.countLines && !isFound; i++)
 	{
 		// Если отсутсвуют строки в выходном параметре с определением искомой функции
-		if(funcDefinition->countLines == 0)
+		if (funcDefinition->countLines == 0)
 		{
 			//isWordInLine(programText.ptrOnLines[i], targetFuncName)
 			if (isLineHeadOfDefinition((char*)programText.ptrOnLines[i], targetFuncName))
@@ -82,4 +132,33 @@ bool findFunctionDefinition(const Text programText, const char targetFuncName[MA
 		}
 	}
 	return isFound;
+}
+
+
+int main()
+{
+	char s[] = "void name((), , ()=)";
+	//bool p = isCompareLineWithPattern(s, "name(,)#");
+	bool p = isLineHeadOfDefinition(s, "name");
+	printf("%d\n", p);
+	/*char inputText[][MAX_LINE_LENGTH+1] =
+	{
+		"int main(a)",
+		"{",
+		"}",
+	};
+	char targetFuncName[81] = "main";
+
+	Text programText = { {}, 3 };
+	for (int i = 0; i < programText.countLines; i++)
+		programText.ptrOnLines[i] = (char*)inputText[i];
+
+	Text funcDefinition = {};
+
+	bool resOfSearch = findFunctionDefinition(programText, targetFuncName, &funcDefinition);
+	printf("%d\n", resOfSearch);
+	for (int i = 0; i < funcDefinition.countLines; i++)
+	{
+		printf("%s\n", funcDefinition.ptrOnLines[i]);
+	}*/
 }
